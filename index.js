2a1940d7291efe5d9c14fc3f7efdc238af118948
baltz <baltazar.parra@fliper.app>
2019-04-30 06:11:55 -0300

    Count insta followers

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
-import { getHTML, getTwitterFollowers } from './lib/scraper'
+import { getHTML, getTwitterFollowers, getInstagramFollowers } from './lib/scraper'
 
 async function go() {
-  const html = await getHTML('https://twitter.com/baltzparra')
+  const html = await getHTML('https://twitter.com/karoldalencar')
   const twitterCount = await getTwitterFollowers(html)
   console.log(`You have ${twitterCount} followers`)
+
+  const instaHTML = await getHTML('https://instagram.com/wesbos')
+  const instagramFollowers = await getInstagramFollowers(instaHTML)
 }
 
 go()
